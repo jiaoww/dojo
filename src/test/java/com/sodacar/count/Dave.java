158f11a873f145dfb3084506424ad0f4d47050f6
Evin <weiwei.jiao@sodacar.com>
2018-05-07 20:15:02 +0800

    测试java文件编写

---
+++ src/test/java/com/sodacar/count/Dave.java
@@ -0,0 +1,11 @@
+package com.sodacar.count;
+
+import java.io.File;
+
+//This file Contains 3 lines of code
+public interface Dave {
+    /**
+     * count a number of lines in a file
+     */
+    int countLines(File inFile);//not the real signature!
+}
---
+++ src/test/java/com/sodacar/count/Hello.java
@@ -0,0 +1,12 @@
+package com.sodacar.count;
+
+/******
+ * This is a test program with 5 lines of code
+ *  \/* no nesting allowed
+ *//*****//*****/// Silightly pathological omment ending....
+public class Hello {
+    public static void mian(String[] args){//gotta love java
+        //Say Hello
+        System./*wait*/out./*for*/println/*it*/("Hello");
+    }
+}
